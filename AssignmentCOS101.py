f9c7846745b4634260dc0887cc1d7cfa5df1e81a
nwa713 <nwankwovictor789@gmail.com>
2024-11-19 19:33:22 +0100

    Update AssignmentCOS101.py

--- AssignmentCOS101.py
+++ AssignmentCOS101.py
@@ -1,79 +1,60 @@
-def calculate_velocity(distance,time):
-        return distance/time
-
-def calculate_acceleration(velocity,time):
-    return velocity/time
-
-def area_of_a_circle(r):
-    return float(2 * 3.14* (r*r))
-
-def gravitational_force(m1,m2,G,r):
-    return float((m1*m2*G)/(r*r))
-
-def work_done(time,force,distance):
-    return float((force*distance)/(time))
-
-options = """
-1. velocity
-2. acceleration
-3.area of a circle
-4.gravitational force
-5.work_done"""
-
-prompt='What do you want to solve'
-print(options)
-print(prompt)
-
-question=int(input("Enter the number of the equation you want to solve:"))
+def velocity():
+    print("Calculating velocity...")
+    distance = float(input("Enter distance (m): "))
+    time = float(input("Enter time (s): "))
+    print(f"Velocity = {distance / time} m/s")
 
 
-
-if question == 1:
-    print('Enter the following parameters to calculate velocity.')
-
-    distance=float(input('Enter the specified distance here:'))
-    time=int(input('Enter the specified time here:'))
-
-    print(calculate_velocity(distance,time))
-
-elif question == 2:
-    print("Enter the following parameters to calculate acceleration.")
-
-    velocity=float(input('Enter the specified velocity here:'))
-    time=int(input('Enter the specified time here:'))
-
-    print(calculate_acceleration(velocity,time))
-
-elif  question == 3 :
-    print("Enter the following parameters to calculate the area of a circle.")
-
-    radius=float(input("Enter the specified radius here:."))
-
-    print(area_of_a_circle(radius))
-
-elif  question == 4:
-    print("Enter the following parameters to calculate gravitational force.")
-
-    m1=int(input('Enter the first mass here:'))
-    m2=int(input('Enter the second mass here:'))
-    G=int(input('Enter the gravity here:'))
-    radius=float(input('Enter the radius here:'))
-
-    print(gravitational_force(m1.m2.G.r))
-
-elif  question == 5 :
-    print("Enter the following parameters to calculate work done below.")
-
-    time=int(input('Enter the specified time here:'))
-    distance=float(input('Enter the specified distance here:'))
-    force=float(input('Enter the specified force here:'))
-
-    print(work_done(time,force,distance))
+def force():
+    print("Calculating force...")
+    mass = float(input("Enter mass (kg): "))
+    acceleration = float(input("Enter acceleration (m/s²): "))
+    print(f"Force = {mass * acceleration} N")
 
 
-else:
-    print("Ooops!!This is not part of the given options to select from.Try again..")
+def kinetic_energy():
+    print("Calculating kinetic energy...")
+    mass = float(input("Enter mass (kg): "))
+    velocity = float(input("Enter velocity (m/s): "))
+    print(f"Kinetic Energy = {0.5 * mass * velocity ** 2} J")
 
 
+def potential_energy():
+    print("Calculating potential energy...")
+    mass = float(input("Enter mass (kg): "))
+    height = float(input("Enter height (m): "))
+    gravity = 9.8  # acceleration due to gravity
+    print(f"Potential Energy = {mass * gravity * height} J")
 
 
+def power():
+    print("Calculating power...")
+    work = float(input("Enter work done (J): "))
+    time = float(input("Enter time taken (s): "))
+    print(f"Power = {work / time} W")
+
+
+def main():
+    print("Choose a physics operation:")
+    print("a. Velocity")
+    print("b. Force")
+    print("c. Kinetic Energy")
+    print("d. Potential Energy")
+    print("e. Power")
+
+    choice = input("Enter your choice (a-e): ").lower()
+    if choice == 'a':
+        velocity()
+    elif choice == 'b':
+        force()
+    elif choice == 'c':
+        kinetic_energy()
+    elif choice == 'd':
+        potential_energy()
+    elif choice == 'e':
+        power()
+    else:
+        print("Invalid choice. Please enter a valid option.")
+
+
+# Call the main function to start the program
